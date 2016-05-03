b3ae4e562717380eae1d487e67ca40443918549a
euglazer <eugenestapler@gmail.com>
2016-05-03 12:24:14 -0700

    refactor network

--- js/network/index.js
+++ js/network/index.js
@@ -1,34 +1,35 @@
 import randomColor from 'randomcolor'
 import NodeCluster from './node-cluster.js'
 import range from 'lodash.range'
+import times from 'times-loop'
 
 class Network {
 
-  constructor (opts) {
+  constructor (opts = {}) {
+    this.opts = opts
     this.audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-
     this.audioOut = this.audioCtx.createGain()
     this.audioOut.gain.value = 0.3
     this.audioOut.connect(this.audioCtx.destination)
 
-    this.scene = opts.scene
-    this.boundingBoxSize = opts.boundingBoxSize
-    this.nodeClusterCount = opts.nodeClusterCount || 10
-    this.nodeClusterSize = opts.nodeClusterSize || 3
-    this.nodeClusters = range(this.nodeClusterCount).map(() => {
-      return new NodeCluster({
-        size: this.nodeClusterSize,
-        color: randomColor({luminosity: 'random'}),
-        scene: this.scene,
-        boundingBoxSize: this.boundingBoxSize,
-        audioCtx: this.audioCtx,
-        audioOut: this.audioOut
-      })
-    })
+    this.nodeClusters = []
+    times(opts.nodeClusterCount || 10, this.addCluster.bind(this))
   }
 
   render () {
     this.nodeClusters.forEach(cluster => cluster.render())
+  }
+
+  addCluster () {
+    var cluster = new NodeCluster({
+      size: this.opts.nodeClusterSize || 3,
+      color: randomColor({luminosity: 'random'}),
+      scene: this.opts.scene,
+      boundingBoxSize: this.opts.boundingBoxSize,
+      audioCtx: this.audioCtx,
+      audioOut: this.audioOut
+    })
+    this.nodeClusters.push(cluster)
   }
 
 }
